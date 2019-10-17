5b61fa1dd613b1f437df6de077ede640be95f240
Jen Lindner <jenlindner22@gmail.com>
2019-10-17 04:40:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ monopoly = {
     :rent_in_dollars => {
       :one_piece_owned => 25,
       :two_pieces_owned => 50,
-      :three_pieces_owned => {},
+      :three_pieces_owned => 100,
       :four_pieces_owned => {}
 },
     :names => {
